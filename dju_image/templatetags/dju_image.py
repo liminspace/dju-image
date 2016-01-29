613ae30dfdbb24d90a5b98c01e1818b9201ce549
liminspace <igormeln@gmail.com>
2016-01-29 04:30:40 +0200

    Templatetag fix.

--- dju_image/templatetags/dju_image.py
+++ dju_image/templatetags/dju_image.py
@@ -8,4 +8,6 @@ register = template.Library()
 
 @register.simple_tag
 def dju_img_url(img_id, label=None, ext=None):
+    if not img_id:
+        return ''
     return settings.MEDIA_URL + get_relative_path_from_img_id(img_id, variant_label=label, ext=ext)
